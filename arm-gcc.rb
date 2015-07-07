c17f546d7d1f3a8e38b45260da33f22f69917627
Ladislas de Toldi <ladislas@leka.io>
2015-07-07 19:00:04 +0200

    add version number and move the toolchain to the right place

--- arm-gcc.rb
+++ arm-gcc.rb
@@ -1,17 +1,15 @@
 require 'formula'
 
-# print avr-gcc's builtin include paths
-# `avr-gcc -print-prog-name=cc1plus` -v
-
-class AvrGcc < Formula
+class ArmGcc < Formula
 
     homepage 'https://launchpad.net/gcc-arm-embedded'
     url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2'
     sha256 '48841185eefa482f0338059dd779b3edf832521ccd05811d557ef4a2807b8284'
+    version '4.9.3'
 
     def install
-
-
+        bin.install Dir["bin/*"]
+        prefix.install Dir["arm-none-eabi", "lib", "share"]
     end
 end
 
