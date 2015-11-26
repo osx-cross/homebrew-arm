8afd1ec2cc23354d1f5f7159309488281ebb1797
Ladislas de Toldi <ladislas@leka.io>
2015-11-26 09:35:19 +0100

    Update binaries to 4.9-2015-q3-update release

--- arm-gcc.rb
+++ arm-gcc.rb
@@ -3,8 +3,8 @@ require 'formula'
 class ArmGcc < Formula
 
     homepage 'https://launchpad.net/gcc-arm-embedded'
-    url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2'
-    sha256 '48841185eefa482f0338059dd779b3edf832521ccd05811d557ef4a2807b8284'
+    url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2'
+    sha256 'a6353db31face60c2091c2c84c902fc4d566decd1aa04884cd822c383d13c9fa'
     version '4.9.3'
 
     def install
