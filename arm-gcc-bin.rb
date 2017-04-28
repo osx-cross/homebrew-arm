6570bc1fdce1437dbbda6cf62da0b81966d0184e
Tim Strazzere <strazz@gmail.com>
2017-04-28 07:38:11 -0700

    Fix to arm.com link

--- arm-gcc-bin.rb
+++ arm-gcc-bin.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ArmGccBin < Formula
 
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
-    url 'https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-mac.tar.bz2'
+    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-mac.tar.bz2'
     sha256 'de4de95b09740272aa95ca5a43bb234ba29c323eddcad2ee34e901eebda910a2'
     version '6-2017-q1-update'
 
