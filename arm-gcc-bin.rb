9cc6b641e89f8357038fe6884e3e237ba8f4a279
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2018-12-21 00:04:43 +0100

    Update to 8-2018-q4-major release

--- arm-gcc-bin.rb
+++ arm-gcc-bin.rb
@@ -3,9 +3,9 @@ require 'formula'
 class ArmGccBin < Formula
 
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
-    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-mac.tar.bz2'
-    sha256 'c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759'
-    version '7-2018-q2-update'
+    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-mac.tar.bz2'
+    sha256 '0b528ed24db9f0fa39e5efdae9bcfc56bf9e07555cb267c70ff3fee84ec98460'
+    version '8-2018-q4-major'
 
     def install
         bin.install Dir["bin/*"]
