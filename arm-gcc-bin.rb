4d6621d553d5f9d78dc125fd43ff9e160123cc5b
Mike M <mikester70387@gmail.com>
2019-11-04 16:12:37 -0500

    Added archive link of latest release (8-2019-q3-update) as the CDN link is not available at the moment.

--- arm-gcc-bin.rb
+++ arm-gcc-bin.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ArmGccBin < Formula
 
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
-    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2'
+    url 'https://web.archive.org/web/20191015145024if_/https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2'
     sha256 'fc235ce853bf3bceba46eff4b95764c5935ca07fc4998762ef5e5b7d05f37085'
     version '8-2019-q3-update'
 
