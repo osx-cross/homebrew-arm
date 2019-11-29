d2ffbe7987db532f4951dae6b91b0acbc85d4789
fauxpark <fauxpark@gmail.com>
2019-11-29 19:10:18 +1100

    Add back 8-2019-q3-update as a separate formula (#15)
    * Add back 8-2019-q3-update as a separate formula
    * Mark as keg only
    * Closes #14

--- arm-gcc-bin.rb
+++ arm-gcc-bin.rb
@@ -1,16 +1,15 @@
 require 'formula'
 
 class ArmGccBin < Formula
-	desc 'GNU Arm Embedded Toolchain - Pre-built GNU toolchain for Arm Cortex-M and Cortex-R processors'
+    desc 'GNU Arm Embedded Toolchain - Pre-built GNU toolchain for Arm Cortex-M and Cortex-R processors'
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
-    
+
     url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/RC2.1/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2'
     sha256 '1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0'
-    version '8-2019-q3-update'
+    version '9-2019-q4-major'
 
     def install
         bin.install Dir["bin/*"]
         prefix.install Dir["arm-none-eabi", "lib", "share"]
     end
 end
-
