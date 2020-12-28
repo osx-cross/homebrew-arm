c319aad439519c1ae6d60a2af3c3b1689bf023f4
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2020-12-28 18:39:51 +0100

    Update to 10-2020-q4-major (#16)

--- arm-gcc-bin.rb
+++ arm-gcc-bin.rb
@@ -4,10 +4,10 @@ class ArmGccBin < Formula
     desc 'GNU Arm Embedded Toolchain - Pre-built GNU toolchain for Arm Cortex-M and Cortex-R processors'
     homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm'
 
-    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-mac.tar.bz2'
+    url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2'
 
-    sha256 'bbb9b87e442b426eca3148fa74705c66b49a63f148902a0ea46f676ec24f9ac6'
-    version '9-2020-q2-update'
+    sha256 'bed12de3565d4eb02e7b58be945376eaca79a8ae3ebb785ec7344e7e2db0bdc0'
+    version '10-2020-q4-major'
 
     def install
         bin.install Dir["bin/*"]
