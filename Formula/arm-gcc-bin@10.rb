a0384f36c9058f884560bcbfcc08d3cd733e128f
Christopher Durand <christopher.durand@rwth-aachen.de>
2021-12-06 02:44:04 +0100

    Update to 10.3-2021.10 release

--- Formula/arm-gcc-bin@10.rb
+++ Formula/arm-gcc-bin@10.rb
@@ -1,10 +1,9 @@
 class ArmGccBinAT10 < Formula
   desc "Pre-built GNU toolchain for Arm Cortex-M and Cortex-R processors"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm"
-  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3-2021.07/gcc-arm-none-eabi-10.3-2021.07-mac-10.14.6.tar.bz2"
-  version "10.3-2021.07"
-  sha256 "0a4554b248a1626496eeba56ad59d2bba4279cb485099f820bb887fe6a8b7ee4"
-  revision 1
+  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2"
+  version "10.3-2021.10"
+  sha256 "fb613dacb25149f140f73fe9ff6c380bb43328e6bf813473986e9127e2bc283b"
 
   bottle do
     root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@10-10.3-2021.07_1"
