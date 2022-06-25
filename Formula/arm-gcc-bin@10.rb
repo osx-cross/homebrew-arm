f6e1b56591e3b5c157879ad95bb42fe8314c9c03
Ladislas de Toldi <ladislas@detoldi.me>
2022-06-25 20:32:53 +0200

    Bump revision

--- Formula/arm-gcc-bin@10.rb
+++ Formula/arm-gcc-bin@10.rb
@@ -5,6 +5,8 @@ class ArmGccBinAT10 < Formula
   version "10.3-2021.10"
   sha256 "fb613dacb25149f140f73fe9ff6c380bb43328e6bf813473986e9127e2bc283b"
 
+  revision 1
+
   bottle do
     root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@10-10.3-2021.10"
     sha256 cellar: :any_skip_relocation, big_sur: "bf9b941c1436e2d9ce39b8f30e55253f5d1284f5b8b1506012c3c3aa9e132d1f"
