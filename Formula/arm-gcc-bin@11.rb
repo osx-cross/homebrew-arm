e05d16be291afb5ffd90370973e8f0be31bda1ce
osxCrossTestBot <osxCrossTestBot@leka.io>
2022-10-17 15:36:03 +0000

    arm-gcc-bin@11: add 11.3.Rel1 bottle.

--- Formula/arm-gcc-bin@11.rb
+++ Formula/arm-gcc-bin@11.rb
@@ -5,6 +5,12 @@ class ArmGccBinAT11 < Formula
   version "11.3.Rel1"
   sha256 "826353d45e7fbaa9b87c514e7c758a82f349cb7fc3fd949423687671539b29cf"
 
+  bottle do
+    root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@11-11.3.Rel1"
+    sha256 cellar: :any_skip_relocation, big_sur:  "8d40a8dfd259c43b45f7a7bc8c6b8e7105c5395ee05b41dfd3228628e6d2e15d"
+    sha256 cellar: :any_skip_relocation, catalina: "1479060c84d3c0581bdbf0aef78834f77d7d5b4dd7678366c1e3a883dd4ddbd6"
+  end
+
   def install
     bin.install Dir["bin/*"]
     prefix.install Dir["arm-none-eabi", "include", "lib", "libexec", "share"]
