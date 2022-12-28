1f49ee593210b2b11a88ff55fb4cbde0aa24e87c
osxCrossTestBot <osxCrossTestBot@leka.io>
2022-12-28 17:14:46 +0000

    arm-gcc-bin@12: add 12.2.Rel1 bottle.

--- Formula/arm-gcc-bin@12.rb
+++ Formula/arm-gcc-bin@12.rb
@@ -2,6 +2,12 @@ class ArmGccBinAT12 < Formula
   desc "Pre-built GNU toolchain for Arm Cortex-M and Cortex-R processors"
   homepage "https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads"
   url "https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/arm-gnu-toolchain-12.2.rel1-darwin-x86_64-arm-none-eabi.tar.xz"
+
+  bottle do
+    root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@12-12.2.Rel1"
+    sha256 cellar: :any_skip_relocation, big_sur:  "7163811bf21aa6a06f4f57aeac31c07a2c604c85ba20d962086b85c42ae9502d"
+    sha256 cellar: :any_skip_relocation, catalina: "bc98e405bd431134f8becab57cedc80792650f4e0462cacff4e06fa612b2bd03"
+  end
   url "https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/arm-gnu-toolchain-12.2.rel1-darwin-arm64-arm-none-eabi.tar.xz" if Hardware::CPU.arm?
   version "12.2.Rel1"
   sha256 "00c0eeb57ae92332f216151ac66df6ba17d2d3b306dac86f4006006f437b2902"
