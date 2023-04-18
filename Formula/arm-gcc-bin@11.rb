bf43d7d904e2ec0e0f6f563c04e660d17c3fbea5
osxCrossTestBot <osxCrossTestBot@leka.io>
2023-04-18 15:49:32 +0000

    arm-gcc-bin@11: update 11.3.Rel1 bottle.

--- Formula/arm-gcc-bin@11.rb
+++ Formula/arm-gcc-bin@11.rb
@@ -7,8 +7,9 @@ class ArmGccBinAT11 < Formula
 
   bottle do
     root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@11-11.3.Rel1"
-    sha256 cellar: :any_skip_relocation, big_sur:  "8d40a8dfd259c43b45f7a7bc8c6b8e7105c5395ee05b41dfd3228628e6d2e15d"
-    sha256 cellar: :any_skip_relocation, catalina: "1479060c84d3c0581bdbf0aef78834f77d7d5b4dd7678366c1e3a883dd4ddbd6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:  "49ed3e200709cc90d43850d651b167529265002854ad522960e36b041a51400f"
+    sha256 cellar: :any_skip_relocation, catalina: "6ff4b710cdc595aac9ea0af7dcb3c86b16424a45de0fe789def3603f5f34f9df"
   end
 
   keg_only <<~EOS
