9b6e3137e41df4db5b84b138977bf7bec9df59b8
Ladislas de Toldi <ladislas@detoldi.me>
2023-06-06 09:23:45 +0200

    :bookmark: (arm-gcc): Bump arm-gcc@12 to Version 12.2.MPACBTI-Rel1

--- Formula/arm-gcc-bin@12.rb
+++ Formula/arm-gcc-bin@12.rb
@@ -1,20 +1,20 @@
 class ArmGccBinAT12 < Formula
   @tar_file = if Hardware::CPU.arm?
-    "arm-gnu-toolchain-12.2.rel1-darwin-arm64-arm-none-eabi.tar.xz"
+    "arm-gnu-toolchain-12.2.MPACBTI-Rel1-darwin-arm64-arm-none-eabi.tar.xz"
   else
-    "arm-gnu-toolchain-12.2.rel1-darwin-x86_64-arm-none-eabi.tar.xz"
+    "arm-gnu-toolchain-12.2.MPACBTI-Rel1-darwin-x86_64-arm-none-eabi.tar.xz"
   end
 
   @tar_file_sha = if Hardware::CPU.arm?
-    "21a9e875250bcb0db8df4cb23dd43c94c00a1d3b98ecba9cdd6ed51586b12248"
+    "0569c8379e3335a8de104ef0d19f0b79c8644c571a9aa005049f0300664a68a1"
   else
-    "00c0eeb57ae92332f216151ac66df6ba17d2d3b306dac86f4006006f437b2902"
+    "febcb19108a400576a7cfa312b46c2393b78ab41cfcc450d219e9485b0d8e375"
   end
 
   desc "Pre-built GNU toolchain for Arm Cortex-M and Cortex-R processors"
   homepage "https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads"
 
-  url "https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/#{@tar_file}"
+  url "https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-rel1/binrel/#{@tar_file}"
   version "12.2.Rel1"
 
   sha256 @tar_file_sha
