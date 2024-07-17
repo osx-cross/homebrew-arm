fb3cbb53825acf7e496a7c69fcbd0649140c27cd
Ladislas de Toldi <ladislas@detoldi.me>
2024-07-17 15:28:48 +0200

    :fire: (gcc@13): Remove bottles block

--- Formula/arm-gcc-bin@13.rb
+++ Formula/arm-gcc-bin@13.rb
@@ -18,13 +18,6 @@ class ArmGccBinAT13 < Formula
   url "https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/#{@tar_file}"
   sha256 @tar_file_sha
 
-  bottle do
-    root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@13-13.3.rel1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "91ec58e4f00cf911ed4d12534591b776e9b1217ceb3b5b5f582da76646f70de7"
-    sha256 cellar: :any_skip_relocation, ventura:      "6e8834c9c8bd67023b438451588ac679b2c520139ff9daec0692f184923078ec"
-    sha256 cellar: :any_skip_relocation, monterey:     "b2ea7549fb70b7a69438a814c84bd9ef637b33ee93ec135842e82b787aa1db34"
-  end
-
   depends_on "xz" unless Hardware::CPU.arm?
   depends_on "zstd" unless Hardware::CPU.arm?
 
