f503183f68b6a2b37891c9990ef31b179ad8ae9a
osxCrossTestBot <osxCrossTestBot@leka.io>
2026-03-09 06:59:28 +0000

    arm-gcc-bin@15: add 15.2.rel1 bottle.

--- Formula/arm-gcc-bin@15.rb
+++ Formula/arm-gcc-bin@15.rb
@@ -6,6 +6,13 @@ class ArmGccBinAT15 < Formula
   url "https://developer.arm.com/-/media/Files/downloads/gnu/15.2.rel1/binrel/arm-gnu-toolchain-15.2.rel1-darwin-arm64-arm-none-eabi.tar.xz"
   sha256 "1938a84b7105c192e3fb4fa5e893ba25f425f7ddab40515ae608cd40f68669a8"
 
+  bottle do
+    root_url "https://github.com/osx-cross/homebrew-arm/releases/download/arm-gcc-bin@15-15.2.rel1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ad29ace172c668a25900cec50e0e1f762fecfdbea9dd394c9f1de41f57cccac"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "879cd211418a95969420c6a1800c23830c3e57e99cde1663e480f5ab46310310"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f9473b45b70109b12c0976b5248d69c60365816c2813fd73006308a4f63e18fb"
+  end
+
   keg_only <<~KEG_ONLY_EOS
     it may interfere with another version of arm-gcc-bin.
     This is useful if you want to have multiple versions installed
